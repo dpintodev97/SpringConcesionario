63a248537b8ea10a3019a427fcc224ebe43fd9bf
DAVID <davidpintodev97@gmail.com>
2024-07-07 17:40:28 +0200

    first commit con archivo .gitignore (carpetas: target y .settings)

---
+++ src/main/java/com/crmConce/springConcesionario/SpringConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package com.crmConce.springConcesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConcesionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConcesionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crmConce/springConcesionario/SpringConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmConce.springConcesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConcesionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
